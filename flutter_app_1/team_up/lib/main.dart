079db6f78ba02825ecf46833f0030e1c3a6c8176
BrightLaw9 <46764863+BrightLaw9@users.noreply.github.com>
2023-05-07 16:27:33 -0400

    add Push notification configuration

--- flutter_app_1/team_up/lib/main.dart
+++ flutter_app_1/team_up/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:team_up/screens/signin_screen.dart';
+import 'package:team_up/services/push_notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await PushNotificationService.initialize();
   runApp(const MyApp());
 }
 
---
+++ flutter_app_1/team_up/lib/services/push_notification_service.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService {
+  static final FirebaseMessaging firebase_messenger =
+      FirebaseMessaging.instance;
+
+  final CHANNEL_ID = "Notification";
+
+  static Future initialize() async {
+    if (Platform.isIOS) {
+      NotificationSettings settings =
+          await firebase_messenger.requestPermission();
+      print("User granted permission: ${settings.authorizationStatus}");
+    }
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("Received a message: ${message.notification}");
+      //NotificationChannel channel =
+    });
+
+    FirebaseMessaging.onBackgroundMessage((RemoteMessage message) async {
+      print(
+          "Received a message while running in the background: ${message.notification}");
+    });
+  }
+}
