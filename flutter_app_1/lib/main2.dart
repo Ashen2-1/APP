596b162c1670414fad163aa419ada080734b0252
Ashen2-1 <Changxianli205@gmail.com>
2023-06-10 13:19:01 -0400

    home_page

---
+++ flutter_app_1/lib/main2.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_1/file_uploader.dart';
+import 'package:flutter_app_1/screens/welcome_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
